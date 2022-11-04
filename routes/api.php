5021b1962c98fcfdc658b4c772d9b4dfc63e5ea0
nguyenbinhminh99 <minhmocmeo0@gmail.com>
2022-11-04 14:10:43 +0700

    JWT login

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('user', [\App\Http\Controllers\AuthController::class, 'user']);
+    Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
 });
+
+
+Route::post('register', [\App\Http\Controllers\AuthController::class, 'register']);
+
+Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
